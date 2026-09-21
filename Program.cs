81c7549cc61988a42f699b321adf094f0a4117ea
Jakubso616 <jakublangiewicz130@gmail.com>
2026-09-21 12:36:01 +0200

    wylosowanie i wypisanie tablicy

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿Console.WriteLine("Hello, World!");
+// Tablica 6 wartosci losowych
+// funkcja zwraca tablice przekazujemy do niej liczbe elementow 
+
+int[] wylosujTablice(int liczbaElementow)
+{
+    int[] tablicaWylosowana = new int[liczbaElementow]; 
+    for (int i = 0; i < tablicaWylosowana.Length; i++)
+    {        
+        tablicaWylosowana[i] = new Random().Next(1, 100); 
+    }
+    return tablicaWylosowana;
+}
+void wypiszTablice(int[] tablicaDoWypisania)
+{     for (int i = 0; i < tablicaDoWypisania.Length; i++)
+    {
+        Console.Write(tablicaDoWypisania[i] + ", ");
+    }
+   
+}
+int[] tablicaProbna = wylosujTablice(10);
+wypiszTablice(tablicaProbna);
