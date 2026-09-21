f647e3193e5748ccfe2d1d22f099639c1cc9f181
Jakubso616 <jakublangiewicz130@gmail.com>
2026-09-21 13:31:52 +0200

    losowanie listy bez powtorzen

--- Program.cs
+++ Program.cs
@@ -20,3 +20,49 @@ void wypiszTablice(int[] tablicaDoWypisania)
 }
 int[] tablicaProbna = wylosujTablice(10);
 wypiszTablice(tablicaProbna);
+
+int[] wczytajLiczbyDoTablicy(int ileLiczb)
+{
+    int[] tablicaZKlawiatury = new int[ileLiczb];
+    for (int i = 0; i < ileLiczb; i++)
+    {
+        Console.WriteLine("Podaj liczbe: ");
+        tablicaZKlawiatury[i] = int.Parse(
+            Console.ReadLine());
+    }
+    return tablicaZKlawiatury;
+}
+int[] tablicaWczytana = wczytajLiczbyDoTablicy(6);
+wypiszTablice(tablicaWczytana);
+
+List<int> wylosowanieLiczbBezPowtórzenDOListy(int ileLiczba)
+{
+    List<int> listaWylosowana = new List<int>();
+    // w listach mozna zmieniac rozmiar
+    // w trakcie działania programu
+    Random random = new Random();
+   for(int i = 0; i < ileLiczba; i++)
+    {
+        int liczba = random.Next(1, 10);
+        while (listaLiczbLosowych.Contains(liczba))
+        {
+            liczba = random.Next(1, 10);
+        }
+        {
+            liczba = random.Next(1, 10);
+        }
+        listaWylosowana.Add(liczba);
+    }
+    return listaWylosowana;
+}
+void wypiszListe(List<int> listaliczb) { 
+
+    {
+        for (int i = 0; i < listaliczb.Count; i++)
+        {
+            Console.Write(listaliczb[i] + ", ");
+        }
+}
+}
+List<int> listaProbna = wylosowanieLiczbBezPowtórzenDOListy(10);
+wypiszListe(listaProbna);w
